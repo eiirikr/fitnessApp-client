b401a8cc62fef37def89ed9f8e093e63b080f246
javierjericm <javierjericm@gmail.com>
2025-01-23 22:37:08 +0800

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,4 @@
 import Banner from "../components/Banner";
-import { useState, useEffect } from "react";
-import { Notyf } from "notyf";
 
 export default function Home() {
   const data = {
@@ -9,29 +7,6 @@ export default function Home() {
     destination: "/login",
     buttonLabel: "Login to get Started",
   };
-
-  const [workouts, setWorkouts] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const notyf = new Notyf();
-    setLoading(true);
-
-    // Fetch workouts
-    fetch(`${process.env.REACT_APP_API_BASE_URL}/workouts/getMyWorkouts`)
-      .then((res) => res.json())
-      .then((data) => setWorkouts(data))
-      .catch((err) => {
-        notyf.error("Error fetching data");
-        console.error(err);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
-  if (loading) {
-    return <div className="text-center mt-5">Loading...</div>;
-  }
 
   return (
     <>
